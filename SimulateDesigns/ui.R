a382fc61f0a1973bc60fadfb51b2215a62aab490
Albert Sorribas <alsote@gmail.com>
2025-11-22 18:47:43 +0100

    version 0.0.2

--- SimulateDesigns/ui.R
+++ SimulateDesigns/ui.R
@@ -9,6 +9,7 @@ ui <- dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem("Home", tabName = "home", icon = icon("tachometer-alt")),
+      # menuItem("Confidence intervals", tabName = "CI", icon = icon("chart-bar")),
       menuItem("Two-arms designs", tabName = "two-arms", icon = icon("chart-bar")),
       menuItem("Multi-arms designs", tabName = "multi-arms", icon = icon("chart-bar")),
       menuItem("Longitudinal designs", tabName = "longitudinal", icon = icon("chart-bar"))
@@ -22,10 +23,29 @@ ui <- dashboardPage(
               
              
       ),
+      tabItem(tabName = "CI",
+              h3("Confidence intervals")),
       # Segunda pestaña
       tabItem(tabName = "two-arms",
-              box(title="Parallel two-arms clinical trial: importance of the control group",width = 7,
+              fluidRow(
+                column(8,
+                       h3(HTML("A two-arms clinical trial is a type of clinical study where participants 
+                       are divided into two groups (or “arms”):
+                       <br><br>
+                       <ul>
+                       <li>Treatment Group (Arm 1): Participants receive the experimental treatment or intervention.</li>
+                       <li>Control Group (Arm 2): Participants may receive a placebo, standard treatment, 
+                       or no intervention.</li>
+                       </ul><br>
+                               These trials are common in evaluating the efficacy and safety of new drugs,
+                               medical devices, or treatment protocols by directly comparing outcomes between 
+                               the two arms.")))
+              ),
+              hr(),
+              box(title="Parallel two-arms clinical trial: importance of the control group",width = 12,
                   status="primary",solidHeader = T,collapsible = T, collapsed = T,
+                  fluidPage(
+                    column(7,
                   h3(HTML("In a simple two-arm clinical trial without baseline measurements, 
                           differences observed at the end of the study may be influenced by pre-existing 
                           imbalances rather than the treatment effect itself. Without accounting for participants’ 
@@ -42,9 +62,15 @@ ui <- dashboardPage(
                     class = "btn btn-success",
                     "Go to simulator"
                   )),
-              box(title="Parallel two-arms clinical trial with binary outcome",width = 7,
+                  column(5,
+                         img(src = "img/FigureRCT.jpg", width = "100%") )
+                  )
+                  ),
+              box(title="Parallel two-arms clinical trial with binary outcome",width = 12,
                   status="primary",solidHeader = T,collapsible = T, collapsed = T,
-                  h3(HTML("n a two-arm design with a binary outcome, the choice between superiority, e
+                  fluidPage(
+                    column(7,
+                  h3(HTML("In a two-arm design with a binary outcome, the choice between superiority, e
                   quivalence, and non-inferiority frameworks determines how treatment effects are evaluated.
                   <ul><li>Superiority trials aim to show that a new intervention performs significantly better 
                   than the control, typically testing whether the difference in event rates exceeds zero in a f
@@ -64,7 +90,12 @@ ui <- dashboardPage(
                     target = "_blank",
                     class = "btn btn-success",
                     "Go to simulator"
-                  ))
+                  )),
+                  
+                  column(5,
+                         img(src = "img/Superiority.png", width = "100%") )
+                  )
+                  )
       )
     )
   )
